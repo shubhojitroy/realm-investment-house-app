3beaaf173f8e9571d5a543ad0b9fac7177d847d0
shubhojitroy <royshubhojit@gmail.com>
2025-01-19 21:39:50 +1100

    update ng.json package app-configfile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+  ],
 };
